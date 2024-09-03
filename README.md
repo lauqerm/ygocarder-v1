86366f3ecae57c7d3d101c60aaf710ef4091ec9c
lauqerm <mr.equal1996@gmail.com>
2024-09-03 12:41:47 +0700

    feat: migrate v1

---
+++ README.md
@@ -0,0 +1,39 @@
+This is a V1 version of Yugioh Carder app. It is a legacy repo and therefore will not receive any active support anymore.
+
+If you are looking for V2, it is located at [ygocarder repo](https://github.com/lauqerm/ygocarder).
+
+## YGO Carder
+
+* Template provided by: [Grezar](https://www.deviantart.com/grezar).
+* GUI by me, a.k.a @lauqerm
+
+### Thank you
+
+My utmost appreciation to [Grezar](https://www.deviantart.com/grezar), for allowing me to use their templates. Their hard works are the reason this project is possible in the first place.
+
+### My goal
+
+Create an easy-to-use GUI to make Yu-gi-oh cards, for anyone who cannot afford Photoshop or the skill needed to use it.
+
+### What does it provide?
+
+To put it more correctly: what advantage does my app offer over a dozen Yu-gi-oh card-maker apps out there? For short, it is just 2 things:
+* The UI - Most of changes can be made with just a single click or keystroke. Other apps will need you to constantly cycle through multiple dropdowns.
+* Automatic text compression - You may notice modern Yu-gi-oh cards try to compress words to avoid using smaller font sizes. YGO Carder can replicate that behavior rather automatically while other apps will just simply keep reducing font size and adding new lines, or provide you a manual slider without solving any edge cases.
+
+Other advantages such as foils and additional card frames come from the template of use, so again big shout out to template owners who put their time and effort into creating them.
+
+### What does it NOT provide?
+
+The app of course has multiple shortcomings, many of them are by design, while others may become future plans:
+* No template for Rush Duel - Template for Rush Duel is already available, so maybe I will revisit this idea in the future.
+* No "ultra" card size - While some apps may offer 4K resolution for their card, this app does not. While I think the current card resolution of my app is decently enough, you can always seek for professional up-scale method.
+* No remote saving - This app does not have a dedicated server for such feature, but thanks to that I can serve the app for a long time completely free without the need for any finacial support. The app only provides very simple card information export and import, and your card art should come from online links.
+
+### Can I use your source code in my site?
+
+Yes, you are free to fork it and do whatever, just don't claim it is yours. Also, I will be extra thankful if you keep the credit for me and template makers.
+
+If you want to modify app's assets, better contact template makers and ask for permission yourself.
+
+You may contact me through [Reddit](https://www.reddit.com/user/lauqerm/), but I rarely respond in time though, I only visit reddit once in a while.
---
+++ src/page/app-header/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+import { Popover } from 'antd';
+import './app-header.scss';
+
+export const AppHeader = React.memo(() => {
+    return <div className="app-header">
+        <img alt="app-logo" src={`${process.env.PUBLIC_URL}/logo192.png`} width={48} />
+        <div className="app-description">
+            <h1>
+                Yugioh Carder <small style={{ fontFamily: 'serif' }}>
+                    v{process.env.REACT_APP_VERSION ?? 'unknown'} TCG
+                </small> - <a className="v2" href="https://lauqerm.github.io/ygocarder">V2 is available!</a></h1>
+            <div className="app-contribution">
+                <span>GUI: <b>Lauqerm</b></span>
+                <span>Template: <b>Grezar</b> and others (<a
+                    href="https://www.deviantart.com/grezar/art/SKILL-UPDATE-Series-10-Card-Proxy-Template-686736691"
+                    target="_blank"
+                    rel="noreferrer">Deviant Art</a>)</span>
+                <Popover overlayClassName="disclaimer-overlay" content={<div className="disclaimer">
+                    <h2>Disclaimer</h2>
+                    <ul>
+                        <li>This is a personal project, not an "official" card maker.</li>
+                        <li>I holds no liability for cards created by this app, including picture, card text and any product that related to it.</li>
+                        <li>I do not own any of Yu-Gi-Oh card layers and fonts use in this project.</li>
+                        <li>If you want to modify card layers, or use it in any public project, please contact <a
+                            href="https://www.deviantart.com/grezar"
+                            target="_blank"
+                            rel="noreferrer">Grezar</a> directly.</li>
+                    </ul>
+                </div>}>
+                    <ExclamationCircleOutlined className="disclaimer-icon" />
+                </Popover>
+            </div>
+        </div>
+    </div>;
+}, () => true);
+
+export const taintedCanvasWarning = <Popover
+    overlayClassName="disclaimer-overlay"
+    style={{ display: 'inline-block' }}
+    content={<div className="disclaimer">
+        <h2>What is happening?</h2>
+        <p>
+        You are using an image host that does not get the "approval" needed to be draw to the app. Browsers see it as a potential security risk (even if it is not the case), and mark the canvas as "tainted".
+        </p>
+        <h2>How does it affect me?</h2>
+        <p>
+        A "tainted canvas" prevent all sort of automatic save. From then on you must save the card manually by right click it → "Save image as...". It is implying that this action is fully of your concern.<br />
+        Furthemore, once it get tainted, it will remains tainted even if you switch to other "safe" image host.
+        </p>
+        <h2>How do I fix it?</h2>
+        <p>
+        Change to other safe and popular image host ("imgur" for example) <b>then refresh the page</b>.<br />
+        If you cannot afford to do that, the app will also work mostly just fine, you only need to manually save the card (and keep cautious).
+        </p>
+    </div>}>
+    <ExclamationCircleOutlined className="disclaimer-icon" />
+</Popover>;
